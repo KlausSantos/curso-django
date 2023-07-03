5877ac27d3abf0569bb7b6ee40700eb4f48ea53e
KlausSantos <klausantos82@gmail.com>
2023-07-03 14:08:03 -0300

    create partials html and form of search

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 1')
-
-
-def contato(request):
-    return HttpResponse('contato!')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/pages/home.html')
